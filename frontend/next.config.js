a8046f37a776e68f574ad7cdebbc17aa06938564
Xavier Damman <xdamman@gmail.com>
2020-04-08 11:34:54 +0200

    new frontend directory, updated readme

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+module.exports = {
+  env: {
+    // You can also use process.env.API_URL if it is defined in your .env file 
+    API_URL: "https://openletter-earth.herokuapp.com" 
+  },
+}
--- next.config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-module.exports = {
-  env: {
-    // Reference a variable that was defined in the .env file and make it available at Build Time
-    API_URL: process.env.API_URL,
-  },
-}
