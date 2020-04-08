2417fade986adb0aafa6b990d9cb2b4e114e99bd
Xavier Damman <xdamman@gmail.com>
2020-04-08 10:53:15 +0200

    fix

--- api/config/database.js
+++ api/config/database.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
