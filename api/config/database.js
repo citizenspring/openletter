3ef4caab523d08187967042cb4010f2266961af9
Xavier Damman <xdamman@gmail.com>
2020-05-02 13:37:17 +0200

    script to download production database

--- api/config/database.js
+++ api/config/database.js
@@ -80,7 +80,7 @@ module.exports = {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      ssl:true
+      ssl: process.env.NODE_ENV === 'production'
     }
   }
 }
