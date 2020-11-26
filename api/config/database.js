a801c1a8035dd7136f908daf6a3ef5f038ceb9bd
Xavier Damman <xdamman@gmail.com>
2020-11-26 15:33:43 +0100

    added rejectUnauthorized for postgres heroku

--- api/config/database.js
+++ api/config/database.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const Url = require('url-parse')
+const Url = require('url-parse');
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use('Env');
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use('Helpers');
 
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'));
 
 module.exports = {
   /*
@@ -36,9 +36,9 @@ module.exports = {
   sqlite: {
     client: 'sqlite3',
     connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   /*
@@ -58,8 +58,8 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      database: Env.get('DB_DATABASE', 'adonis'),
+    },
   },
 
   /*
@@ -80,7 +80,8 @@ module.exports = {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      ssl: process.env.NODE_ENV === 'production'
-    }
-  }
-}
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      // }process.env.NODE_ENV === 'production'
+    },
+  },
+};
