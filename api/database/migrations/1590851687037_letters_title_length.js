6c7002e7f2a3e96aa4f51df1df694c3a74931b49
Xavier Damman <xdamman@gmail.com>
2022-04-01 14:18:32 +0200

    change column length title and slug from 100 to 256

--- api/database/migrations/1590851687037_letters_title_length.js
+++ api/database/migrations/1590851687037_letters_title_length.js
@@ -6,14 +6,14 @@ const Schema = use('Schema')
 class LetterSchema extends Schema {
   up () {
     this.table('letters', (table) => {
-      table.string('slug', 255).notNullable().unique().index().alter()
+      table.string('slug', 255).notNullable().index().alter()
       table.string('title', 255).notNullable().alter()
     })
   }
 
   down () {
     this.table('letters', (table) => {
-      table.string('slug', 100).notNullable().unique().index().alter()
+      table.string('slug', 100).notNullable().index().alter()
       table.string('title', 100).notNullable().alter()
     })
   }
