998bbdbd42e314f7ce40c4b20db7de360c2b9051
Xavier Damman <xdamman@gmail.com>
2022-04-01 12:31:59 +0200

    change column length title and slug from 100 to 256

---
+++ api/database/migrations/1590851687037_letters_title_length.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('letters', 'title', { type: Sequelize.DataTypes.STRING(256) });
+    await queryInterface.changeColumn('letters', 'slug', { type: Sequelize.DataTypes.STRING(256) });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('letters', 'title', { type: Sequelize.DataTypes.STRING(100) });
+    await queryInterface.changeColumn('letters', 'slug', { type: Sequelize.DataTypes.STRING(100) });
+  }
+}
+
+module.exports = LetterSchema
