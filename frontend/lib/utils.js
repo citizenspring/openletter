e2b58087d26a43e71af2fe1dbfbe52f4f29b41ba
Xavier Damman <xdamman@gmail.com>
2023-12-26 11:29:32 +0100

    fix for url regex

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -1,9 +1,10 @@
 export function replaceURLsWithMarkdownAnchors(text) {
-  const regex = /((http?s:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gi;
+  const regex = /((http?s:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z]{2,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gi;
   return text.replace(regex, (match) => {
     // Check if the URL starts with http/https, if not, prepend 'http://'
     let url = match.startsWith('http') ? match : `http://${match}`;
-    return `[${match}](${url})`;
+    const anchor = match.length > 50 ? `${match.replace(/https?:\/\/(www\.)?/i, '').substring(0, 50)}...` : match;
+    return `[${anchor}](${url})`;
     // return `<a href="${url}">${match}</a>`;
   });
 }
