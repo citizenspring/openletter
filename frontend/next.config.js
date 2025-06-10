83529a9085682924fede80ecfaf38f54e2a2532c
Xavier Damman <xdamman@gmail.com>
2025-06-10 17:14:22 +0200

    move out of heroku

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ var locales = require('./constants/locales');
 module.exports = {
   env: {
     // You can also use process.env.API_URL if it is defined in your .env file
-    API_URL: process.env.API_URL || 'https://openletter-earth.herokuapp.com',
+    API_URL: process.env.API_URL || 'https://api.openletter.earth',
     OC_GRAPHQL_API: 'https://api.opencollective.com/graphql/v1/',
     OC_GRAPHQL_API_V2: 'https://api.opencollective.com/graphql/v2/',
   },
